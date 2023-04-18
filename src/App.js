72255fab484a86cc7fc2c8b9ea290d7049024bea
Yavor99 <yavor_enchev99@abv.bg>
2023-04-18 17:36:35 +0300

    Add more Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from "./components";
 
 function App() {
     return (
         <div className="App">
-            <h1>GPT-3</h1>
+            <div className="gradient_bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     );
 }
